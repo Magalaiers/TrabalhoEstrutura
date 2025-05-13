820455c583ded90c924471b9e6354bed9507a059
Lucas Ameda <yutameda@gmail.com>
2025-05-13 20:04:27 -0300

    Experimental 2
    Experimental 2

--- TrabalhoEstrutura.c
+++ TrabalhoEstrutura.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World!");
+    printf("Hello World!\n");
+
+    int x1, x2, soma;
+    printf("\nDigite um número: ");
+    scanf("%d", &x1);
+    printf("Digite outro número: ");
+    scanf("%d", &x2);
+    soma = x1 + x2;
+    printf("%d + %d = %d", x1, x2, soma);  
 }
