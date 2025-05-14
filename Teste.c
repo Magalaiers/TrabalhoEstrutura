37b2d5443490bbb490a6f09d2b460a1d96ac2657
Lucas Ameda <yutameda@gmail.com>
2025-05-14 14:52:54 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+//#include <stdio.h>
 
-int main(){
-    printf("Hello World!\n");
+//int main(){
+    //printf("Hello World!\n");
 
-    int x1, x2, soma;
-    printf("\nDigite um número: ");
-    scanf("%d", &x1);
-    printf("Digite outro número: ");
-    scanf("%d", &x2);
-    soma = x1 + x2;
-    printf("%d + %d = %d", x1, x2, soma);  
-}
+    //int x1, x2, soma;
+    //printf("\nDigite um número: ");
+    //scanf("%d", &x1);
+    //printf("Digite outro número: ");
+    //scanf("%d", &x2);
+    //soma = x1 + x2;
+    //printf("%d + %d = %d", x1, x2, soma);  
+//}
